96891396d90cc755c1597a9591c053b96fca3294
JesBrian <JesBrian666@gmail.com>
2018-03-09 19:11:23 +0800

    界面 [ 简单构建更新资料页 ]

--- Vue-Dev/src/views/type/update.vue
+++ Vue-Dev/src/views/type/update.vue
@@ -1,8 +1,52 @@
 <template>
   <div>
-    <div class="glass-bg box-show" style="width:92%; min-height:688px; margin:0 auto;">
-
-    </div>
+    <table class="glass-bg box-show" style="width:95%; margin:0 auto; padding:18px 0; color:#DDD;">
+      <tr>
+        <td style="height:80px; position:relative;">
+          <div class="cube-bg" style="width:62px; height:62px; top:0; right:8px; position:absolute; display:inline-block;">
+            <img class="box-show" v-lazy="this.$store.state.baseHost + 'img/touxiang.jpg'" style="width:88%; height:88%; margin-top:6%;"/>
+          </div>
+        </td>
+        <td>
+          <p style="margin-top:-12px; font-size:22px; font-weight:700;">User Name牛逼</p>
+        </td>
+      </tr>
+      <tr>
+        <td style="width:88px; padding-bottom:20px;">标题</td>
+        <td style="padding-bottom:18px"><label><input type="text" class="cube-bg box-show" style="width:80%; margin:0 auto; padding:5px 8px; font-size:20px; color:#DDD;"/></label></td>
+      </tr>
+      <tr>
+        <td style="width:88px; padding-bottom:20px;">标题</td>
+        <td style="padding-bottom:18px"><label><input type="text" class="cube-bg box-show" style="width:80%; margin:0 auto; padding:5px 8px; font-size:20px; color:#DDD;"/></label></td>
+      </tr>
+      <tr>
+        <td style="width:88px; padding-bottom:20px;">标题</td>
+        <td style="padding-bottom:18px"><label><input type="text" class="cube-bg box-show" style="width:80%; margin:0 auto; padding:5px 8px; font-size:20px; color:#DDD;"/></label></td>
+      </tr>
+      <tr>
+        <td style="width:88px; padding-bottom:20px;">标题</td>
+        <td style="padding-bottom:18px"><label><input type="text" class="cube-bg box-show" style="width:80%; margin:0 auto; padding:5px 8px; font-size:20px; color:#DDD;"/></label></td>
+      </tr>
+      <tr>
+        <td style="width:88px; padding-bottom:20px;">标题</td>
+        <td style="padding-bottom:18px"><label><input type="text" class="cube-bg box-show" style="width:80%; margin:0 auto; padding:5px 8px; font-size:20px; color:#DDD;"/></label></td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <div class="box-show" style="width:90%; margin:0 auto; min-height:288px;"></div>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2" style="padding-top:18px;">
+          <span class="super-btn-out" style="width:108px; height:33px; margin:0 6px;">
+            <span class="super-btn-in MyIF cancel" style="width:98px; height:23px; top:48%; line-height:24px;"> 取消返回</span>
+          </span>
+          <span class="super-btn-out" style="width:108px; height:33px; margin:0 6px;">
+            <span class="super-btn-in MyIF makesure" style="width:98px; height:23px; top:48%; line-height:24px;"> 确定更新</span>
+          </span>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
